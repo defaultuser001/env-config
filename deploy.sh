7094d5765cc02981cb947092835c9c74fa2a1d3a
Pablo Quarella <pquarella@gmail.com>
2020-12-04 02:15:06 -0300

    automatic update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cp ~/vim-config/.vimrc ~/
-cp ~/vim-config/.vimrc.local ~/
+mkdir ~/.vim
+mkdir ~/.vim/autoload
+mkdir ~/.vim/plugins
+cp ~/vim-config/plug.vim ~/.vim/autoload
+
+echo  ' En vim:: PlugInstall '
+echo  ' Luego ejecutar "for_linux_after_deploy.sh '
--- sync.sh
+++ sync.sh
@@ -1,12 +1,8 @@
 sudo cp ~/.vimrc /root
-sudo cp ~/.vimrc.local /root
-
 sudo cp -r ~/.vim/ /root
 
 cp ~/.vimrc ~/vim-config
-cp ~/.vimrc.local ~/vim-config
-
-cd ~/vim-config
+cd  ~/vim-config
 
 git add .
 git commit -am "automatic update"
