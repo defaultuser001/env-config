a4dc4b2419ddc29dc0635d671286502b08ca39aa
Pablo Quarella <pquarella@caldensecurity.com>
2021-03-14 10:45:47 -0300

    automatic update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ mkdir ~/.vim
 mkdir ~/.vim/autoload
 mkdir ~/.vim/plugins
 cp ~/env-config/plug.vim ~/.vim/autoload
-sudo apt  install -y clangd build-essential vim-gtk cmake python-dev python3-dev
+sudo apt  install -y python3-pip clangd build-essential vim-gtk cmake python-dev python3-dev
+
+pip3 install pep8
+pip3 install autopep8
 
 #Terminator
 sudo apt install terminator
